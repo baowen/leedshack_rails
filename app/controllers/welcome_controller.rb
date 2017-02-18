0df0ef1bd3260055a612cd3f84fcac31a37d3c06
Ben Owen <ben.owen@dvla.gsi.gov.uk>
2017-02-18 15:28:55 +0000

    add some pages

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,30 @@
 class WelcomeController < ApplicationController
+
+  def page1
+  end
+
+  def page1_post
+
+    redirect_to welcome_page2_path
+  end
+
+  def page2
+  end
+
+  def page2_post
+  end
+
+
+
+
+  private
+
+  def handle_post
+    if request.post? && self.respond_to?( "#{action_name}_post" )
+      self.public_send "#{action_name}_post"
+    end
+    yield unless performed?
+  end
+
+
 end
